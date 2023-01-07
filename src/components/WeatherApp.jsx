82a3c99092c34376c6cee6a9c8b2b6d6be515d43
Jonathan <jonato96s@gmail.com>
2023-01-07 16:31:27 -0500

    Get data from api

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react"
+import WeatherForm from "./WeatherForm"
+
 const WeatherApp = () => {
+    const [weather, setWeather] = useState(null)
+
+    async function loadInfo(city = "quito"){        
+        try{
+            const request = await fetch(`
+                ${process.env.REACT_APP_URL}
+                &key=${process.env.REACT_APP_KEY}
+                &q=${city}
+                `)
+            const json = await request.json()
+            console.log(json)
+            setWeather(json)
+        }catch(error){
+            console.log("Error:"+error)
+        }
+
+    }
+
+    function handleChangeCity(city){
+        setWeather(null)
+        loadInfo()
+    }
     return(
         <div>
-            Hola
+            <WeatherForm onChangeCity={handleChangeCity}/>          
+            <div>
+                {weather?.current.temp_c}
+            </div>
         </div>
+
     )
 }
 export default WeatherApp
